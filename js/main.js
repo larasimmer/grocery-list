93c87423ec71293a72fa45900bd601914760bc3e
Lara Simmer <larafsimmer@gmail.com>
2022-08-16 20:29:01 -0300

    Ativando eventos nos botoes de expandir e minimizar

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
 //VARIÁVEIS
 
-const buttonMeat = $("#meat > .category-expand");
-const buttonDairy = $("#dairy > .category-expand");
-const buttonDriedGoods = $("#gried-goods > .category-expand");
-const buttonVeggies = $("#veggies > .category-expand");
-const buttonBread = $("#bread > .category-expand");
-const buttonCleaners = $("#cleaners > .category-expand");
-const buttonCare = $("#care > .category-expand");
-const buttonBeverages = $("#beverages > .category-expand");
-const buttonOthers = $("#others > .category-expand");
+const expandButton = $(".category-expand");
+const minimizeButton = $(".category-minimize");
 
 //CHAMANDO FUNÇÕES
 
-$(".category").on("click", function() {
-    $(this).css("height", "300px");
+expandButton.on("click", function() {
+    $(this).parent().parent().parent().css("height", "300px");
+})
+
+minimizeButton.on("click", function() {
+    $(this).parent().parent().parent().css("height", "80px");
 })
 
 //DECLARANDO FUNÇÕES
