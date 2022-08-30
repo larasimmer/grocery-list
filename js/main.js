baf635040b7e30eb6b168b850a172fbdb93b811f
Lara Simmer <larafsimmer@gmail.com>
2022-08-30 20:16:12 -0300

    Adding items functionality

--- js/main.js
+++ js/main.js
@@ -2,15 +2,22 @@
 
 const expandButton = $(".category-expand");
 const minimizeButton = $(".category-minimize");
+const addItemButton = $(".add-item");
+const addItemInput = $(".item-input");
+let addedItem = $(".added-item");
 
 //CHAMANDO FUNÇÕES
 
 expandButton.on("click", function() {
-    $(this).parent().parent().parent().css("height", "300px");
+    $(".add-items-container").show();
 })
 
 minimizeButton.on("click", function() {
-    $(this).parent().parent().parent().css("height", "80px");
+    $(".add-items-container").hide();
+})
+
+addItemButton.on("click", function() {
+    addItems.append("<ul class='items'><ul class='items-actions'><li class='checkbox'><input type='checkbox' id='check' value='bought'></li><li class='added-item'></li><li><i class='fa-solid fa-pen-to-square'></i></li><li><i class='fa-solid fa-xmark'></i></li></ul></ul><div><input class='item-input' type='text' id='item'><button class='add-item'>Add</button></div>");
 })
 
 //DECLARANDO FUNÇÕES
